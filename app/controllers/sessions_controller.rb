aa8e2cb61e71c050cbde9bc201cd743f809d70ab
daniel-cutright <daniel.cutright@kellogg.northwestern.edu>
2026-02-26 03:33:58 +0000

    Completed last 60 minutes of class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,8 @@ class SessionsController < ApplicationController
     
     # 3. if they know their password -> login is successful
       if BCrypt::Password.new(@user["password"]) == params["password"]
+        # add cookie with the user's id
+        session["user_id"] = @user["id"]
         flash["notice"] = "Welcome."
         redirect_to "/companies"
       else
@@ -27,6 +29,7 @@ class SessionsController < ApplicationController
 
   def destroy
     # logout the user
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
